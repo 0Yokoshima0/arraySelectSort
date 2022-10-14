771177144e8e80cb539ab7b604849904d203fa1b
Yokoshima <112453676+0Yokoshima0@users.noreply.github.com>
2022-10-14 16:31:21 +0800

    Add files via upload

---
+++ selectSort.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class selectSort {
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Please input the length of an array.");
+        int total = Integer.parseInt(input.nextLine());
+        int[] arr = new int[total];
+        System.out.println("please input the values of each keys");
+        for(int i = 0; i < arr.length;i++ ){
+            int num = input.nextInt();
+            arr[i] = num;
+        }
+        System.out.println("The array is: ");
+        printArrays(arr);
+        System.out.println("The sort of the array is: ");
+        selectSort(arr);
+        printArrays(arr);
+    }
+
+    //defin
+    public static void swap(int[] arr, int i , int j ){
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+    public static void printArrays(int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void selectSort(int[] arr){
+        if(arr == null || arr.length < 2) {
+            return;
+        }
+        for(int i = 0; i < arr.length; i++){
+            int minValueIndex = i;
+            for(int j = i + 1; j < arr.length; j++ ){
+                minValueIndex = arr[minValueIndex] < arr[j] ? minValueIndex : j;
+            }
+            swap(arr,minValueIndex, i);
+        }
+    }
+}
